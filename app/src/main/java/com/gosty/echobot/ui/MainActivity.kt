adcf04bae6ee69495dc2541767e46f003faca84a
Ghifari Octaverin <ghifariocta21@gmail.com>
2024-09-11 14:07:48 +0700

    feat: Splashscreen

--- app/src/main/java/com/gosty/echobot/ui/MainActivity.kt
+++ app/src/main/java/com/gosty/echobot/ui/MainActivity.kt
@@ -1,17 +1,58 @@
 package com.gosty.echobot.ui
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.gosty.echobot.features.splash.SplashViewModel
 import com.gosty.echobot.ui.theme.EchoBotTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen().apply {
+            setOnExitAnimationListener { viewProvider ->
+                ObjectAnimator.ofFloat(
+                    viewProvider.view,
+                    "scaleX",
+                    0.5f, 0f
+                ).apply {
+                    interpolator = OvershootInterpolator()
+                    duration = 300
+                    doOnEnd { viewProvider.remove() }
+                    start()
+                }
+                ObjectAnimator.ofFloat(
+                    viewProvider.view,
+                    "scaleY",
+                    0.5f, 0f
+                ).apply {
+                    interpolator = OvershootInterpolator()
+                    duration = 300
+                    doOnEnd { viewProvider.remove() }
+                    start()
+                }
+            }
+        }
+
         super.onCreate(savedInstanceState)
+
+        splashScreen.setKeepOnScreenCondition {
+            viewModel.isSplashShow.value
+        }
+
         setContent {
             EchoBotTheme {
                 // A surface container using the 'background' color from the theme
